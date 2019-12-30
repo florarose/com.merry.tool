84c5276af00e8faae00fb568ef0d6c2d0a190d86
merryLdt <17835059864@163.com>
2019-12-30 18:49:42 +0800

    提交测试

---
+++ Study_Thread/src/main/java/com/java/thread/executor/Notify.java
@@ -0,0 +1,14 @@
+package com.java.thread.executor;
+
+/**
+ * @author 坎布里奇
+ * @version 1.0
+ * @date 2019/11/13 15:34
+ */
+public interface Notify {
+
+    /**
+     * 回调
+     */
+    void notifyListen() ;
+}
